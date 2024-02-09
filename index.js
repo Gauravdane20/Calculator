1fd944db807e521abcf6c0ae193ba5c3419bb7a9
Gaurav Dane <140782128+Gauravdane20@users.noreply.github.com>
2024-02-09 15:25:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+function Solve(value){
+  var v = document.getElementById("display");
+  v.value += value;
+}
+
+function Result(){
+    var num1 = document.getElementById("display").value;
+    var num2 = eval(num1);
+    document.getElementById("display").value = num2;
+  }
+
+  function Clear(){
+    var inp = document.getElementById("display");
+    inp.value = "";
+  }
+
+  function Back(){
+    var ev = document.getElementById("display");
+    ev.value = ev.value.slice(0, -1);
+  }
